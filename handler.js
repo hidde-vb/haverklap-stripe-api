40e5b373d031a1848f6cf396d0b2e4615e0e4b8c
hiddevb <hiddevanbavel@hotmail.com>
2022-11-16 20:47:43 +0100

    basic implementation

--- handler.js
+++ handler.js
@@ -1,17 +1,33 @@
-const serverless = require("serverless-http");
-const express = require("express");
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import serverless from "serverless-http";
+import express from "express";
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+
 const app = express();
 
 app.get("/", (req, res, next) => {
   return res.status(200).json({
-    message: "Hello from root!",
+    message: "Alive",
   });
 });
 
-app.get("/hello", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from path!",
+app.post("/create-checkout-session", async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        price: "pr_1234",
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    success_url: `${YOUR_DOMAIN}?success=true`,
+    cancel_url: `${YOUR_DOMAIN}?canceled=true`,
   });
+
+  res.redirect(303, session.url);
 });
 
 app.use((req, res, next) => {
@@ -20,4 +36,4 @@ app.use((req, res, next) => {
   });
 });
 
-module.exports.handler = serverless(app);
+export const handler = serverless(app);
